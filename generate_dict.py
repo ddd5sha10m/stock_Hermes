279237221ab0385ab5885ed43b8c1c4c8ad11db2
wdwddaniel <wdwddaniel@gmail.com>
2025-10-02 14:34:33 +0800

    增加上櫃股票

--- generate_dict.py
+++ generate_dict.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # 讀取 CSV 檔案 (請改成你的檔名，例如 "stocks.csv")
-df = pd.read_csv("StockList.csv", encoding="utf-8")
+df = pd.read_csv("StockList1.csv", encoding="utf-8")
 
 # 取出股票代號與名稱
 stock_dict = dict(zip(df["代號"].astype(str), df["名稱"].str.strip()))
 
 # 將字典存成一個 .py 檔案
-with open("stocks_dict.py", "w", encoding="utf-8") as f:
+with open("stocks_dict1.py", "w", encoding="utf-8") as f:
     f.write("STOCK_DICT = {\n")
     for code, name in stock_dict.items():
         f.write(f'"{code}": "{name}",\n')
